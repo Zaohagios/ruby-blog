a837c7ff4333db75e4d84e995454bd9e2887d856
William Young <williamharoldyoung@gmail.com>
2016-01-19 04:22:49 -0500

    Updated Ignore

--- vendor/cache/ruby/2.0.0/bin/thor.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Users/William/Documents/Aptana Studio 3 Workspace/Hello/alpha-blog/vendor/cache/ruby/2.0.0/bin/thor" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"ruby.exe" "%~dpn0" %*
