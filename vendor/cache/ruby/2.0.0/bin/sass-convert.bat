8625438c2bd9b7835d8915ff10193663405cfa45
William Young <williamharoldyoung@gmail.com>
2016-01-19 04:15:34 -0500

    Removed redundancy

---
+++ vendor/cache/ruby/2.0.0/bin/sass-convert.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "C:/Users/William/Documents/Aptana Studio 3 Workspace/Hello/alpha-blog/vendor/cache/ruby/2.0.0/bin/sass-convert" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
